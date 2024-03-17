82c34b06a26b1483d788c46ac3661e7478655856
Rizky Kusuma Nugraha <kusumarizky65@gmail.com>
2024-03-17 19:26:59 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmod5_1302220013/Program.cs
+++ tpmod5_1302220013/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class HaloGeneric
+{
+
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+    static void Main(string[] args)
+    {
+
+        HaloGeneric halo = new HaloGeneric();
+
+        halo.SapaUser("Rizky Kusuma Nugraha");
+
+    }
+
+}
+
